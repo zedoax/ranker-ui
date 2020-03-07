b3ad71e2c7414872cc2f494cebb8d9d921f2e029
zedoax <zedoax@anagrio.net>
2020-03-06 23:11:06 -0500

    Remove logger statements

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import Handler from "./redux/components/handler";
-import { Log } from 'oidc-client';
-
-Log.logger = console;
-Log.level = Log.DEBUG;
-
 
 const App = () => {
   return (
